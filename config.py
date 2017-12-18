d59b2acc077c6a42b26969ef3ffe6249b48f528b
Kaiyuan <chenkaiyuan@ucla.edu>
2017-12-18 15:21:45 -0800

    decidetouseoriginalutils

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ import utils.py
 class Config():
     def __init__():
         self.ae_hop_length = 512
+        self.num_iter = 10000
         self.learning_rate = 0.00001
     def get_batch():
         #TODO:get batch
